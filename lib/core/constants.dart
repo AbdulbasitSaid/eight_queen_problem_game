be6ee5564d30ee3c60f98c2ced22c257c55574c4
Abdulbasit Saeed <abdulbasitsaeed619@gmail.com>
2024-04-22 09:49:26 +0200

    Add Constants class with app name, background images, and number of queens Remove GameTheme class and its dependencies Remove router.dart file and its dependencies

---
+++ lib/core/constants.dart
@@ -0,0 +1,7 @@
+class Constants {
+  static const String appName = 'Eight Queen Problem Game';
+  static const List<String> backgroundImages = [
+    'assets/background_images/bg1.jpg',
+  ];
+  static const int numberOfQueens = 8;
+}
