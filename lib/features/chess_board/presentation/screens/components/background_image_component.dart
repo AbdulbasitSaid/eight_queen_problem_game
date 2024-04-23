c96760df7d1811dcdf7274098fccc58370df9a44
Abdulbasit Saeed <abdulbasitsaeed619@gmail.com>
2024-04-24 01:02:24 +0200

    Add tint color to the background image in BackgroundImageComponent

--- lib/features/chess_board/presentation/screens/components/background_image_component.dart
+++ lib/features/chess_board/presentation/screens/components/background_image_component.dart
@@ -10,18 +10,11 @@ class BackgroundImageComponent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Image(
-      // add tint color to the background image
       color: Colors.black.withOpacity(.4),
       colorBlendMode: BlendMode.darken,
       height: ScreenUtil().screenHeight,
       width: ScreenUtil().screenWidth,
       fit: BoxFit.cover,
-      loadingBuilder: (context, child, loadingProgress) =>
-          loadingProgress == null
-              ? child
-              : const Center(
-                  child: CircularProgressIndicator(),
-                ),
       image: AssetImage(Constants.backgroundImages[1]),
     );
   }
